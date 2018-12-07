e7e7231172fdd3904b3e350ee637796a0d3ab824
João Menezes <j.ribeiro.de.menezes@accenture.com>
2018-12-07 18:44:55 -0200

    add result table data insertion logic

--- scripts/app.js
+++ scripts/app.js
@@ -28,6 +28,7 @@
 
 	document.getElementById('buttonOK').addEventListener('click', function() {
 		
+		var label = document.getElementById('label').value;
 		var balance = document.getElementById('balance').value;
 		var outwardprice = document.getElementById('outwardprice').value;
 		var returnprice = document.getElementById('returnprice').value;
@@ -40,7 +41,7 @@
 		
 		var cardData = {
 			key: key,
-			label: '#' + key,
+			label: label,
 			created: new Date(),
 			balance: balance,
 			outwardprice: outwardprice,
@@ -93,6 +94,7 @@
 			card = app.cardTemplate.cloneNode(true);
 			card.classList.remove('cardTemplate');
 			card.querySelector('.card-title').textContent = '#' + data.key + ' ' + data.label + ' - ' + new Date(data.created).toLocaleString();
+			card.querySelector('.key').textContent = data.key;
 			card.removeAttribute('hidden');
 			app.container.appendChild(card);
 			app.visibleCards[data.key] = card;
@@ -102,13 +104,62 @@
 		card.querySelector('.outwardprice').textContent = data.outwardprice;
 		card.querySelector('.returnprice').textContent = data.returnprice;
 		
-	
+		// calculate recharge options
+		var table = card.querySelector('.resulttable');
+		
+		var rechargeTable = app.calculateBURechargeArray(data);
+		
+		for (let i = 0; i < rechargeTable.length; i++) {
+		
+			var tr =  table.querySelector('.resulttr').cloneNode(true);
+			
+			tr.querySelector('.outwardtickets').textContent = rechargeTable[i].outwardtickets;
+			tr.querySelector('.returntickets').textContent = rechargeTable[i].returntickets;
+			tr.querySelector('.newbalance').textContent = rechargeTable[i].newbalance;
+			tr.querySelector('.creditrecharge').textContent = rechargeTable[i].creditrecharge;
+			tr.removeAttribute('hidden');
+			
+			table.appendChild(tr);
+		}
+		
 		if (app.isLoading) {
 			app.hideSpinner();
 		}
 	};
 
+	
+	/*****************************************************************************
+	 *
+	 * Bussiness Logic
+	 *
+	 ****************************************************************************/
 
+	app.calculateBURechargeArray = function(data) {
+		return [
+			{
+				outwardtickets: 1,
+				returntickets: 1,
+				newbalance: 13.92,
+				creditrecharge: 13.92
+			},
+			{
+				outwardtickets: 2,
+				returntickets: 2,
+				newbalance: 27.84,
+				creditrecharge: 27.84
+			}
+		];
+	}
+
+	var initialBusTicketRecharge = {
+		key: 0,
+		label: 'Example',
+		created: new Date(),
+		balance: 0.00,
+		outwardprice: 6.96,
+		returnprice: 6.96
+	};
+	 
 	/*****************************************************************************
 	 *
 	 * Methods for dealing with the model
@@ -140,15 +191,6 @@
 		return cards;
 	}
 		
-
-	var initialBusTicketRecharge = {
-		key: 0,
-		label: 'Example',
-		created: new Date(),
-		balance: 0.00,
-		outwardprice: 6.96,
-		returnprice: 6.96
-	};
 
 	// Startup code
 	app.cards = app.loadCards();
